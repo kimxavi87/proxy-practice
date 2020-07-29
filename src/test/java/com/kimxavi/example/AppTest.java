c7f6c45cb2b2ca63293ddd55ff4f2fdfb8a1709a
kim-keonwoo <kim.keonwoo@navercorp.com>
2020-07-29 17:38:23 +0900

    dynamic proxy

--- src/test/java/com/kimxavi/example/AppTest.java
+++ src/test/java/com/kimxavi/example/AppTest.java
@@ -4,7 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import com.kimxavi.example.proxy.Subject;
 import com.kimxavi.example.proxy.SubjectProxy;
+import com.kimxavi.example.proxy.SubjectService;
 import org.junit.Test;
+import sun.tools.jconsole.inspector.XObject;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * Unit test for simple App.
@@ -14,7 +20,26 @@ public class AppTest
     Subject subject = new SubjectProxy();
 
     @Test
-    public void proxy() {
+    public void proxyPattern() {
         subject.job();
+    }
+
+    Subject dynamicProxySubject = (Subject) Proxy.newProxyInstance(Subject.class.getClassLoader(), new Class[]{Subject.class}, new InvocationHandler() {
+
+        Subject subject = new SubjectService();
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("dynamic proxy before job");
+            System.out.println(proxy.getClass());
+            Object invoke = method.invoke(subject, args);
+            System.out.println("dynamic proxy after job");
+            return invoke;
+        }
+    });
+
+    @Test
+    public void dynamicProxy() {
+        dynamicProxySubject.job();
     }
 }
